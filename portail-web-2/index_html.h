2f5f11662ee8b9ba0c568aa64707b8746343b940
JVeluz <j.veluz2002@gmail.com>
2026-01-30 18:06:06 +0100

    web server avec LittleFS Les fichiers html sont facilement accessible dans data/

---
+++ portail-web-2/index_html.h
@@ -0,0 +1,21 @@
+const char index_html[] = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portail</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/config">Configuration Caméra</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
+)=====";
