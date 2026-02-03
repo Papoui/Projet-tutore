e956324a7f83cb65ad90ad30bbc469403d1decc8
JVeluz <j.veluz2002@gmail.com>
2026-02-03 15:02:27 +0100

    Update .gitignore to include properties.h and modify WiFi credentials in properties.h

--- portail-web-3/app_httpd.cpp
+++ portail-web-3/app_httpd.cpp
@@ -1307,7 +1307,6 @@ void startCameraServer() {
 #endif
   log_i("Starting web server on port: '%d'", config.server_port);
   if (httpd_start(&camera_httpd, &config) == ESP_OK) {
-    httpd_register_uri_handler(camera_httpd, &config_uri);
     httpd_register_uri_handler(camera_httpd, &cmd_uri);
     httpd_register_uri_handler(camera_httpd, &status_uri);
     httpd_register_uri_handler(camera_httpd, &capture_uri);
--- portail-web-3/properties.h
+++ portail-web-3/properties.h
@@ -1,5 +1,8 @@
 // char SSID[33] = "Bbox-C4D153B1";
 // char PASS[64] = "23F1D67EE1314DEE572346A336A37C";
 
-char SSID[33] = "SFR_6B58";
-char PASS[64] = "38xbgu4tiv76tyah6rgk";
+// char SSID[33] = "SFR_6B58";
+// char PASS[64] = "38xbgu4tiv76tyah6rgk";
+
+char SSID[33] = "Xiaomi 12X";
+char PASS[64] = "6aad43cd";
