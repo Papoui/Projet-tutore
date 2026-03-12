1e6a12f50af15114fe54b86db7073eef86dd93e7
JVeluz <j.veluz2002@gmail.com>
2026-03-12 17:55:52 +0100

    Création du 4eme projet arduino

---
+++ portail-web-4/config_service.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_SERVICE_H
+#define CONFIG_SERVICE_H
+
+#include <EEPROM.h>
+
+struct Config
+{
+    char ssid[32];
+    char password[32];
+    bool loadOnBoot;
+};
+
+const Config DEFAULT_CONFIG = {"ESP32", "123", false};
+
+extern Config myConfig;
+
+#define EEPROM_SIZE sizeof(Config)
+
+void initConfig();
+bool saveConfig();
+void loadFromMemory();
+void resetMemory();
+
+#endif
