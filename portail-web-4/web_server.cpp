72979c93dedd7865be99bd5d41c510aa459bea68
JVeluz <j.veluz2002@gmail.com>
2026-03-15 16:13:46 +0100

    Update web_server.cpp
    Ajout des routes :
    "/" -> index.html
    "/api/config/wifi"
    "/*" -> tous les autres fichiers

--- portail-web-4/web_server.cpp
+++ portail-web-4/web_server.cpp
@@ -3,6 +3,26 @@
 
 AsyncWebServer server(8080);
 
+void handleIndex(AsyncWebServerRequest *request)
+{
+    request->send(LittleFS, "/index.html", "text/html");
+}
+
+void handleWifiConfig(AsyncWebServerRequest *request)
+{
+    if (request->hasParam("ssid"))
+    {
+        String value = request->getParam("ssid")->value();
+        Serial.printf("ssid : %s\n", value.c_str());
+    }
+    if (request->hasParam("password"))
+    {
+        String value = request->getParam("password")->value();
+        Serial.printf("password : %s\n", value.c_str());
+    }
+    request->send(200);
+}
+
 void startWebServer()
 {
     if (!LittleFS.begin(true))
@@ -11,12 +31,11 @@ void startWebServer()
         return;
     }
 
-    server.on("/", HTTP_GET,
-              [](AsyncWebServerRequest *request) { request->send(LittleFS, "/index.html", "text/html"); });
+    server.on("/", HTTP_GET, handleIndex);
+    server.on("/api/config/wifi", HTTP_POST, handleWifiConfig);
 
     server.onNotFound([](AsyncWebServerRequest *request) {
         String path = request->url();
-
         if (LittleFS.exists(path))
         {
             request->send(LittleFS, path);
