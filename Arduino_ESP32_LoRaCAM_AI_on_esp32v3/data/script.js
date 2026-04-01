b249520bb79e720b1d7e6ae7bf14e35823d3e3e2
JVeluz <j.veluz2002@gmail.com>
2026-04-01 11:43:42 +0200

    Ajout des fichiers de l'interface

---
+++ Arduino_ESP32_LoRaCAM_AI_on_esp32v3/data/script.js
@@ -0,0 +1,23 @@
+
+const cameraURL = `http://${window.location.hostname}:80`;
+function onCameraClick(event) {
+    event.preventDefault();
+    window.location.href = cameraURL;
+}
+
+// Fonction générique pour faire un POST avec des données JSON
+async function postHTTP(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+    if (!response.ok) throw new Error(`Erreur HTTP ${response.status} : ${await response.text()}`);
+}
+
+// Fonction générique pour faire un GET et retourner les données JSON
+async function getHTTP(url) {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Erreur HTTP ${response.status} : ${await response.text()}`);
+    return await response.json();
+}
