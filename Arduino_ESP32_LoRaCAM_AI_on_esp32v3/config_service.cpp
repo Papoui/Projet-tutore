35803922a76363a73eed7d4815318171010f2420
JVeluz <j.veluz2002@gmail.com>
2026-04-01 14:34:54 +0200

    config_service utilise lora_config.json
    Au lancement :
    - Chargement en ram des paramètres par défaut dans la var globale myConfig
    - Si lora_config.json existe, on surcharge avec
    
    Pour reset les paramètres il suffit de supprimer le lora_config.json stocker dans l'ESP

--- Arduino_ESP32_LoRaCAM_AI_on_esp32v3/config_service.cpp
+++ Arduino_ESP32_LoRaCAM_AI_on_esp32v3/config_service.cpp
@@ -1,59 +1,114 @@
 #include "config_service.h"
+#include <ArduinoJson.h>
+#include <LittleFS.h>
 
 Config myConfig;
+const char *configFilePath = "/lora_config.json";
+
+const Config DEFAULT_CONFIG = {
+    {
+        "125", // bw
+        "12", // sf
+        "868.1", // fréquence 
+        "26012DAA", // device address
+        "23158D3BBC31E6AF670D195B5AED5525", // AppSKey 
+        "23158D3BBC31E6AF670D195B5AED5525" // NwkSKey
+    },
+    {
+        20, // Quality Factor
+        64 // MSS
+    }
+};
 
 void initConfig()
 {
-    EEPROM.begin(EEPROM_SIZE);
+    if (!LittleFS.begin(true))
+        return;
     loadFromMemory();
-    if (myConfig.loadOnBoot)
-    {
-        Serial.println("Config chargee au demarrage.");
-    }
-    else
-    {
-        Serial.println("Chargement ignore (Option desactivee).");
-        myConfig = DEFAULT_CONFIG;
-    }
-}
-
-void saveConfig()
-{
-    Serial.println("--- Sauvegarde en cours ---");
-    EEPROM.put(0, myConfig);
-    if (EEPROM.commit())
-    {
-        Serial.println("Succès : Données écrites en EEPROM.");
-    }
-    else
-    {
-        Serial.println("Erreur : Échec EEPROM !");
-    }
 }
 
 void loadFromMemory()
 {
-    Serial.println("--- Chargement depuis l'EEPROM ---");
-    EEPROM.get(0, myConfig);
-    Serial.println("Chargement terminé.");
+    myConfig = DEFAULT_CONFIG;
+
+    if (!LittleFS.exists(configFilePath))
+    {
+        return;
+    }
+    
+    File file = LittleFS.open(configFilePath, "r");
+    if (!file)
+    {
+        return;
+    }
+    
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, file);
+    file.close();
+
+    if (error)
+    {
+        return;
+    }
+    
+    myConfig.lora.bw = doc["lora"]["bw"].as<String>();
+    myConfig.lora.sf = doc["lora"]["sf"].as<String>();
+    myConfig.lora.frequency = doc["lora"]["frequency"].as<String>();
+    myConfig.lora.devAddr = doc["lora"]["devAddr"].as<String>();
+    myConfig.lora.appSKey = doc["lora"]["appSKey"].as<String>();
+    myConfig.lora.nwkSKey = doc["lora"]["nwkSKey"].as<String>();
+
+    myConfig.loracam.quality = doc["loracam"]["quality"].as<int>();
+    myConfig.loracam.mss = doc["loracam"]["mss"].as<int>();
 }
 
-void printConfig() {
-    Serial.print("loadOnBoot : ");
-    Serial.println(myConfig.loadOnBoot ? "True" : "False");
-    Serial.print("ssid : ");
-    Serial.println(myConfig.ssid);
-    Serial.print("mot de passe : ");
-    Serial.println(myConfig.password);
-    Serial.print("accessPoint : ");
-    Serial.println(myConfig.accessPoint ? "True" : "False");
+void saveConfig()
+{
+    File file = LittleFS.open(configFilePath, "w");
+    if (!file) 
+    {
+        return;
+    }
+
+    JsonDocument doc;
+    doc["lora"]["bw"] = myConfig.lora.bw;
+    doc["lora"]["sf"] = myConfig.lora.sf;
+    doc["lora"]["frequency"] = myConfig.lora.frequency;
+    doc["lora"]["devAddr"] = myConfig.lora.devAddr;
+    doc["lora"]["appSKey"] = myConfig.lora.appSKey;
+    doc["lora"]["nwkSKey"] = myConfig.lora.nwkSKey;
+    doc["loracam"]["quality"] = myConfig.loracam.quality;
+    doc["loracam"]["mss"] = myConfig.loracam.mss;
+
+    serializeJson(doc, file);
+    file.close();
+}
+
+void printConfig()
+{
+    Serial.print("BW : ");
+    Serial.println(myConfig.lora.bw);
+    Serial.print("SF : ");
+    Serial.println(myConfig.lora.sf);
+    Serial.print("Frequency : ");
+    Serial.println(myConfig.lora.frequency);
+    Serial.print("DevAddr : ");
+    Serial.println(myConfig.lora.devAddr);
+    Serial.print("AppSKey : ");
+    Serial.println(myConfig.lora.appSKey);
+    Serial.print("NwkSKey : ");
+    Serial.println(myConfig.lora.nwkSKey);
+    Serial.print("Quality : ");
+    Serial.println(myConfig.loracam.quality);
+    Serial.print("MSS : ");
+    Serial.println(myConfig.loracam.mss);
 }
 
 void resetMemory()
 {
-    Serial.println("--- Lancement de la réinitialisation de l'EEPROM ---");
-    EEPROM.begin(EEPROM_SIZE);
+    if (LittleFS.exists(configFilePath))
+    {
+        LittleFS.remove(configFilePath);
+    }
     myConfig = DEFAULT_CONFIG;
-    saveConfig();
-    Serial.println("--- EEPROM réinitialisée avec succès ---");
 }
--- Arduino_ESP32_LoRaCAM_AI_on_esp32v3/config_service.h
+++ Arduino_ESP32_LoRaCAM_AI_on_esp32v3/config_service.h
@@ -2,54 +2,36 @@
 #define CONFIG_SERVICE_H
 
 #include <Arduino.h>
-#include <EEPROM.h>
 
-struct Config
+struct LoraConfig
 {
-    // Configuration Générale
-    bool loadOnBoot;
+    String bw;
+    String sf;
+    String frequency;
+    String devAddr;
+    String appSKey;
+    String nwkSKey;
+};
 
-    // WifiConfig
-    char ssid[32];
-    char password[64];
-    bool accessPoint;
-
-    // LoRaConfig
-    int bw;
-    int sf;
-    int frequency;
-    char devAddr[9];  // 8 caractères hexa + '\0'
-    char appSKey[33]; // 32 caractères hexa + '\0'
-    char nwkSKey[33]; // 32 caractères hexa + '\0'
-
-    // LoRaCamConfig
+struct LoraCamConfig
+{
     int quality;
     int mss;
 };
 
-const Config DEFAULT_CONFIG = {
-    false,                              // loadOnBoot
-    "SFR_6B58",                         // ssid
-    "38xbgu4tiv76tyah6rgk",             // password
-    false,                              // accessPoint
-    125,                                // bw
-    12,                                 // sf
-    868100000,                          // frequency
-    "26012DAA",                         // devAddr
-    "23158D3BBC31E6AF670D195B5AED5525", // appSKey
-    "23158D3BBC31E6AF670D195B5AED5525", // nwkSKey
-    20,                                 // quality
-    64                                  // mss
+struct Config
+{
+    LoraConfig lora;
+    LoraCamConfig loracam;
 };
 
 extern Config myConfig;
-
-#define EEPROM_SIZE sizeof(Config)
+extern const Config DEFAULT_CONFIG;
 
 void initConfig();
 void saveConfig();
 void loadFromMemory();
-void resetMemory();
 void printConfig();
+void resetMemory();
 
 #endif
